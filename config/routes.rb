1c6f68fc822b341c39e0c15e6073eef5b925d79c
Gauthier Taton <gauthier_taton@yahoo.fr>
2020-02-27 17:05:03 +0100

    algo ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'cocktails', to: 'cocktails#index'
+  # get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
+  # post 'cocktails', to: 'cocktails#create'
+  # get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:new, :create, :index]
+  end
+  resources :doses, only: [:destroy]
 end
