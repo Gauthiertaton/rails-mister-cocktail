7e9d0fd6496c1465329bd0c3acc1a72bf357b611
Gauthier Taton <gauthier_taton@yahoo.fr>
2020-02-28 12:23:48 +0100

    home page ok

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
   # post 'cocktails', to: 'cocktails#create'
   # get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+  root to: 'cocktails#index'
   resources :cocktails, only: [:index, :show, :create, :new] do
     resources :doses, only: [:new, :create, :index]
   end
