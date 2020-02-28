0e7a3b4f349c95c0c08c7220b4c46f834ea13f7c
Gauthier Taton <gauthier_taton@yahoo.fr>
2020-02-28 12:20:42 +0100

    database ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,18 @@
-require 'json'
-require 'open-uri'
-Ingredient.destroy_all
 Cocktail.destroy_all
+Ingredient.destroy_all
 Dose.destroy_all
-url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients_serialized = open(url).read
-ingredients = JSON.parse(ingredients_serialized)
-ingredients["drinks"].each do |ingredient|
-  Ingredient.create(name: ingredient["strIngredient1"])
-end
-url = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
-cocktails_serialized = open(url).read
-cocktails = JSON.parse(cocktails_serialized)
-cocktails["drinks"].each do |cocktail|
-  Cocktail.create(name: cocktail["strDrink"])
-end
+
+Cocktail.create!(
+  name: 'Mojito'
+)
+
+Cocktail.create!(
+  name: 'Punch'
+)
+
+Cocktail.create!(
+  name: 'Aperol Spritz'
+)
+Cocktail.create!(
+  name: 'Margarita'
+)
